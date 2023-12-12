27a6974b9f5f1e25bb9ebbe2e38cb550cdbe0805
KitsuneAlex <alexh@karmakrafts.dev>
2023-12-12 16:52:48 +0100

    Add initial project files

---
+++ src/main/java/io/karma/ferrous/osmium/Main.java
@@ -0,0 +1,14 @@
+package io.karma.ferrous.osmium;
+
+import org.apiguardian.api.API;
+
+/**
+ * @author Alexander Hinze
+ * @since 12/12/2023
+ */
+@API(status = API.Status.INTERNAL)
+public final class Main {
+    public static void main(final String[] args) {
+
+    }
+}
